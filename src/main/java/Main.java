80e7a360c7672ec8b2a24bde2e84d86548f0289a
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-13 20:44:02 -0700

    2 - Handle User Events with a Button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,14 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
     Button button;
+    Button button2;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,12 +18,26 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Title of the Window");
         button = new Button("Click me");
+        button.setOnAction(this);
+        button2 = new Button("I'm #2");
+        button2.setOnAction(this);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
+        layout.getChildren().add(button2);
 
         Scene scene = new Scene(layout, 300, 150);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == button) {
+            System.out.println("oooo yaaaa keep click right here");
+        }
+        if (event.getSource() == button2) {
+            System.out.println("you clicked button 2");
+        }
     }
 }
