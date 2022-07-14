23878a302f06a6d055969b491d316a969e81a107
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-13 21:44:45 -0700

    5 - Click on a button to create a new stage (that acts as a alert box) and needs to closed before going back to original scene

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     Stage window;
-    Scene scene1, scene2;
+    Button button;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,27 +19,14 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
-
-        Label label1 = new Label("Welcome to the first scene!");
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        // Layout1 - children are laid out in vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
-
-        // Button 2
-        Button button2 = new Button("This scene sucks, go back to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        // Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
         window.setTitle("Title here");
+
+        button = new Button("Click Me");
+        button.setOnAction(eh -> AlertBox.display("Title of Alert Box", "Wow this alert box is awesome)"));
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
         window.show();
     }
 }
