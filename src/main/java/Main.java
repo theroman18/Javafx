4b25ec278c42bcb2b4dc36e053674d1e60db49fc
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-13 21:24:18 -0700

    4 - Switch between 2 scenes by clicking on two buttons with lambda expressions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,14 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -15,24 +18,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the Window");
-        button = new Button("Click me");
-        button.setOnAction(e -> {
-            System.out.println("hey now brown cow");
-            System.out.println("this is what dreams are made of");
-        });
-//        button.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                System.out.println("lets go");
-//            }
-//        });
+        window = primaryStage;
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 150);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        Label label1 = new Label("Welcome to the first scene!");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        // Layout1 - children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        // Button 2
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        // Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
     }
-
 }
