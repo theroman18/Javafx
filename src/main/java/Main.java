30abdac652634f047885c177a9fabe24fda01140
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-13 20:02:41 -0700

    Creating a basic window (aka a stage in Javafx)

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        button = new Button("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 150);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
