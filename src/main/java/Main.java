c54c80880a705bff943b80b82cab740679307a2d
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-14 20:58:02 -0700

    7 - Closes the window properly after clicking on button

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,19 +17,29 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         window = primaryStage;
-        window.setTitle("Title here");
+        window.setTitle("Stage title here");
 
-        button = new Button("Click Me");
-        button.setOnAction(eh -> {
-            boolean result = ConfirmBox.display("Title of Window", "Are you sure you want to send naked pics to ex?");
-            System.out.println(result);
+        // handles the close request if the user clicks on the window's exit button
+        window.setOnCloseRequest(eh -> {
+            // consume() eats the close event, and tells java that we can handle the rest of the event
+            eh.consume();
+            closeProgram();
         });
+
+        button = new Button("Close program");
+        button.setOnAction(eh -> closeProgram());
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
+    }
+    private void closeProgram() {
+        boolean answer = ConfirmBox.display("Stage Title here", "Sure you want to close?");
+        if (answer) {
+            window.close();
+        }
     }
 }
