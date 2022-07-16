12ef2baf7650e6de851de0e9a86146cc261e328a
Roman Sanchez <roman.sanchez1318@gmail.com>
2022-07-16 12:31:33 -0700

    12 - Create a ChoiceBox that lets you select of the items from the list. ChoiceBox is similar to ComboBox but combobox lets you add a CellFactory to it; which is useful for work app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -24,35 +21,27 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Roman's Sandwhiches");
 
-        CheckBox box1 = new CheckBox("Bacon");
-        CheckBox box2 = new CheckBox("Lettuce");
-        CheckBox box3 = new CheckBox("Tomato");
-        box2.setSelected(true);
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
-        button = new Button("Order Now!");
-        button.setOnAction(event -> handleOptions(box1, box2, box3));
+        // getItems returns the ObservableList
+        choiceBox.getItems().addAll("Apples", "Bananas");
+        choiceBox.setValue("Bananas");
+
+        button = new Button("Submit");
+        button.setOnAction(event -> getChoice(choiceBox));
 
         VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(box1, box2, box3, button);
+        layout.setPadding(new Insets(20, 20, 20,20));
+        layout.getChildren().addAll(choiceBox, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-    private void handleOptions(CheckBox box1, CheckBox box2, CheckBox box3) {
-        String message = "Users order:\n";
-
-        if (box1.isSelected()) {
-            message += "Bacon\n";
-        }
-        if (box2.isSelected()) {
-            message += "Lettuce\n";
-        }
-        if (box3.isSelected()) {
-            message += "Tomato\n";
-        }
-        System.out.println(message);
+    private void getChoice(ChoiceBox<String> choiceBox) {
+        String food = choiceBox.getValue();
+        System.out.println(food);
     }
+
 }
